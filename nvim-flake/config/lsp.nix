f33afc31b12ea07ae6b7f34e03cade27e6ccf6a1
Mathieu De Raedt <mathieu@deraedt.dev>
2024-02-04 01:38:41 +0100

    ✨ Add LSP keybinds

--- nvim-flake/config/lsp.nix
+++ nvim-flake/config/lsp.nix
@@ -84,9 +84,78 @@
             cmd = [ "${lexicalPackage}/bin/lexical" ];
           };
         };
+        keymaps = {
+          diagnostic = {
+            "[d" = {
+              action = "goto_prev";
+              desc = "Go to previous diagnostic message";
+            };
+            "]d" = {
+              action = "goto_next";
+              desc = "Go to next diagnostic message";
+            };
+            "<leader>e" = {
+              action = "open_float";
+              desc = "Open floating diagnostic message";
+            };
+            "<leader>q" = {
+              action = "setloclist";
+              desc = "Open diagnostic list";
+            };
+          };
+
+          lspBuf = {
+            "<leader>rn" = {
+              action = "rename";
+              desc = "[R]e[n]ame";
+            };
+            "<leader>ca" = {
+              action = "code_action";
+              desc = "[C]ode [A]ction";
+            };
+            "gd" = {
+              action = "definition";
+              desc = "[G]oto [D]efinition";
+            };
+            "gI" = {
+              action = "implementation";
+              desc = "[G]oto [I]mplementation";
+            };
+            "<leader>D" = {
+              action = "type_definition";
+              desc = "Type [D]efinition";
+            };
+            "K" = {
+              action = "hover";
+              desc = "Hover documentation";
+            };
+            "<c-k>" = {
+              action = "signature_help";
+              desc = "Signature Documentation";
+            };
+            "gD" = {
+              action = "declaration";
+              desc = "[G]oto [D]eclaration";
+            };
+            "<leader>wa" = {
+              action = "add_workspace_folder";
+              desc = "[W]orkspace [A]dd Folder";
+            };
+            "<leader>wr" = {
+              action = "remove_workspace_folder";
+              desc = "[W]orkspace [R]emove Folder";
+            };
+            "<leader>wl" = {
+              action = "list_workspace_folders";
+              desc = "[W]orkspace [L]ist Folders";
+            };
+            "<leader>f" = {
+              action = "format";
+              desc = "Format current buffer with LSP";
+            };
+          };
+        };
       };
-
-
     };
   };
 }
