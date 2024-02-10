7fe9094ba039f945147a6bb3f670cf9ebe2018be
Mathieu De Raedt <mathieu@deraedt.dev>
2024-02-10 01:49:48 +0100

    ✨ Finish UFO

--- nvim-flake/config/ufo.nix
+++ nvim-flake/config/ufo.nix
@@ -9,4 +9,30 @@
       '';
     };
   };
+
+  config.options = {
+    foldcolumn = "1";
+    foldlevel = 99;
+    foldlevelstart = 99;
+    foldenable = true;
+  };
+
+  config.keymaps = [
+    {
+      mode = "n";
+      key = "zR";
+      action.__raw = "require('ufo').openAllFolds";
+      options = {
+        desc = "Fold [R]eveal all";
+      };
+    }
+    {
+      mode = "n";
+      key = "zM";
+      action.__raw = "require('ufo').closeAllFolds";
+      options = {
+        desc = "Fold Close all";
+      };
+    }
+  ];
 }
