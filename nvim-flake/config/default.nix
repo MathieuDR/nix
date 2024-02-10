966e1b6002b24ba5aa37865572c9be4a4bf3b83a
Mathieu De Raedt <mathieu@deraedt.dev>
2024-02-10 01:45:53 +0100

    ✨ UFO

--- nvim-flake/config/default.nix
+++ nvim-flake/config/default.nix
@@ -5,11 +5,12 @@
     ./helpers.nix
     (import ./lsp.nix { inherit lexicalPackage; })
     ./git.nix
+    ./whichkey.nix
     ./fs.nix
     ./no-config-plugins.nix
     ./telescope.nix
     ./trouble.nix
-    ./whichkey.nix
+    ./ufo.nix
     ./keymaps.nix
     ./layout.nix
   ];
---
+++ nvim-flake/config/ufo.nix
@@ -0,0 +1,12 @@
+{
+  config.plugins = {
+    nvim-ufo = {
+      enable = true;
+      providerSelector = ''
+        function(bufnr, filetype, buftype)
+        	return { 'lsp', 'indent' }
+        end
+      '';
+    };
+  };
+}
