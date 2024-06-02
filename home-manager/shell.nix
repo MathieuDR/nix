e08b2af81e96ddc62bf39aa1e55aac21a2f44df6
mathieu <mathieu@deraedt.dev>
2024-06-02 16:16:48 +0200

    🚧 More shell stuff

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   programs = {
     direnv = {
       enable = true;
@@ -6,66 +6,53 @@
       nix-direnv.enable = true;
     };
 
-		oh-my-posh = {
-			enable = true;
-			#settings = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile "${pkgs.oh-my-posh}/share/oh-my-posh/themes/space.omp.json"))
-		};
-
-		jq.enable = true;
-		ripgrep.enable = true;
-		gh.enable = true;
-		fzf.enable = true;
-
-		zoxide = {
-			enable = true;
+    oh-my-posh = {
+      enable = true;
 			enableBashIntegration = true;
-		};
+      #settings = config.home.file.".config/.ysomic.omp.json".source;
+    };
+
+    jq.enable = true;
+    ripgrep.enable = true;
+    gh.enable = true;
+    fzf.enable = true;
+
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+    };
 
     bash = {
       enable = true;
       historySize = 2500;
-      bashrcExtra = [
-        ''
-          function pbcopy() {
-          	xclip -selection clipboard
-          }
+      bashrcExtra = ''
+        function gnb() {
+        		if [ -z "$1" ]; then
+        	   echo "Usage: gnb <branch_name>"
+        	   return 1
+        	 fi
 
-          function pbpaste() {
-          	xclip -selection clipboard -o
-          }
+        	 git checkout -b "$1"
 
-          function pbbranch() {
-          	git branch | grep "*" | awk "{print $2}" | pbcopy() | pbpaste()
-          }
+        	 if [ $? -ne 0 ]; then
+        	   echo "Failed to checkout to branch $1"
+        	   return 1
+        	 fi
 
-          function gnb() {
-          		if [ -z "$1" ]; then
-          	   echo "Usage: gnb <branch_name>"
-          	   return 1
-          	 fi
+        	 git push -u origin "$1"
 
-          	 git checkout -b "$1"
+        	 if [ $? -ne 0 ]; then
+        	   echo "Failed to push the branch $1 to origin"
+        	   return 1
+        	 fi
 
-          	 if [ $? -ne 0 ]; then
-          	   echo "Failed to checkout to branch $1"
-          	   return 1
-          	 fi
+        	 echo "Successfully checked out to and pushed $1"
+        }
 
-          	 git push -u origin "$1"
-
-          	 if [ $? -ne 0 ]; then
-          	   echo "Failed to push the branch $1 to origin"
-          	   return 1
-          	 fi
-
-          	 echo "Successfully checked out to and pushed $1"
-          }
-
-					function docker-compose() {
-					    command docker compose
-					}
-        ''
-      ];
+        function docker-compose() {
+            command docker compose
+        }
+      '';
     };
   };
 }
--- home.nix
+++ home.nix
@@ -55,34 +55,10 @@
 		};
 
     file = {
-			".config/.ysomic.omp.json" = "./home-manager/dotfiles/.ysomic.omp.json";
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
+			".config/.ysomic.omp.json".source = ./home-manager/dotfiles/.ysomic.omp.json;
     };
 
     # Home Manager can also manage your environment variables through
-    # 'home.sessionVariables'. If you don't want to manage your shell through Home
-    # Manager then you have to manually source 'hm-session-vars.sh' located at
-    # either
-    #
-    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  /etc/profiles/per-user/Thieu/etc/profile.d/hm-session-vars.sh
-    #
     sessionVariables = {
       EDITOR = "nvim";
     };
