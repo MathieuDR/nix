c0b590f210a178521b5fa5c76efe7c0c00519701
MathieuDR <mathieu@deraedt.dev>
2024-12-04 15:02:31 +0100

    🔐 keys

--- data/secrets/secrets.nix
+++ data/secrets/secrets.nix
@@ -5,9 +5,11 @@ let
   base = builtins.readFile ./public_keys/id_rsa.pub;
   anchor = builtins.readFile ./public_keys/agenix-anchor-system.pub;
   anchor_thieu = builtins.readFile ./public_keys/agenix-anchor-thieu.pub;
+  wanderer = builtins.readFile ./public_keys/agenix-wanderer-system.pub;
+  wanderer_thieu = builtins.readFile ./public_keys/agenix-wanderer-thieu.pub;
 
-  system_recipients = [anchor base];
-  user_recipients = [anchor_thieu base];
+  system_recipients = [anchor wanderer];
+  user_recipients = [anchor_thieu wanderer_thieu];
   # all_recipients = system_recipients ++ user_recipients;
 
   withBase = recipients: (lib.lists.unique ([base] ++ recipients));
