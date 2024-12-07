169b969c14fd93f4f5fa6dba6ff505cad5aab926
MathieuDR <mathieu@deraedt.dev>
2024-12-07 13:34:32 +0100

    🐛 Correct nesting

--- configuration/home-manager/default.nix
+++ configuration/home-manager/default.nix
@@ -89,17 +89,17 @@
       blueman
       pavucontrol
     ];
-
-    systemd.user.tmpfiles.rules = [
-      # d /path/to/directory MODE USER GROUP AGE ARGUMENT
-      "d ${config.home.homeDirectory}/downloads 0755 ${config.home.username} users - -"
-      "d ${config.home.homeDirectory}/development 0755 ${config.home.username} users - -"
-      "d ${config.home.homeDirectory}/development/sources 0755 ${config.home.username} users - -"
-      "d ${config.home.homeDirectory}/development/courses 0755 ${config.home.username} users - -"
-      "d ${config.home.homeDirectory}/development/sources/sevenmind 0755 ${config.home.username} users - -"
-      "d ${config.home.homeDirectory}/pictures 0755 ${config.home.username} users - -"
-      "d ${config.home.homeDirectory}/pictures/screenshots 0755 ${config.home.username} users - -"
-      "d ${config.home.homeDirectory}/notes 0755 ${config.home.username} users - -"
-    ];
   };
+
+  systemd.user.tmpfiles.rules = [
+    # d /path/to/directory MODE USER GROUP AGE ARGUMENT
+    "d ${config.home.homeDirectory}/downloads 0755 ${config.home.username} users - -"
+    "d ${config.home.homeDirectory}/development 0755 ${config.home.username} users - -"
+    "d ${config.home.homeDirectory}/development/sources 0755 ${config.home.username} users - -"
+    "d ${config.home.homeDirectory}/development/courses 0755 ${config.home.username} users - -"
+    "d ${config.home.homeDirectory}/development/sources/sevenmind 0755 ${config.home.username} users - -"
+    "d ${config.home.homeDirectory}/pictures 0755 ${config.home.username} users - -"
+    "d ${config.home.homeDirectory}/pictures/screenshots 0755 ${config.home.username} users - -"
+    "d ${config.home.homeDirectory}/notes 0755 ${config.home.username} users - -"
+  ];
 }
