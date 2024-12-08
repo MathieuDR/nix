fc53e5a03feae1cf88dfabe9be4b93abb0b7e31d
MathieuDR <mathieu@deraedt.dev>
2024-12-08 23:39:27 +0100

    🐛 Wrong nesting

--- configuration/nixos/optional/default.nix
+++ configuration/nixos/optional/default.nix
@@ -1,9 +1,9 @@
 {
   gaming = import ./gaming.nix;
+  power-management = import ./power-management.nix;
+  wake-up = import ./wake-up.nix;
   programs = {
     onepassword = import ./programs/1password.nix;
     docker = import ./programs/docker.nix;
-    power-management = import ./power-management.nix;
-    wake-up = import ./wake-up.nix;
   };
 }
