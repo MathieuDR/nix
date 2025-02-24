97b261c64c62e5c62cc5bfaa7faff576f63163da
MathieuDR <mathieu@deraedt.dev>
2025-02-24 14:26:56 +0100

    ✨ Trying hyprsplit

--- flake.nix
+++ flake.nix
@@ -54,8 +54,8 @@
       };
     };
 
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
+    hyprsplit = {
+      url = "github:shezdy/hyprsplit";
       inputs.hyprland.follows = "hyprland";
     };
   };
