0e496106e47bdfe33d193770ea7b16424b32c9b9
MathieuDR <mathieu@deraedt.dev>
2025-06-01 21:08:28 +0200

    fsh

--- configuration/home-manager/shell.nix
+++ configuration/home-manager/shell.nix
@@ -5,16 +5,24 @@
   config,
   ...
 }: {
+  home = {
+    shell = {
+      enableFishIntegration = true;
+    };
+  };
+
   programs = {
     direnv = {
       enable = true;
       enableBashIntegration = true;
+      # enableFishIntegration = true; readonly and set to true?
       nix-direnv.enable = true;
     };
 
     oh-my-posh = {
       enable = true;
       enableBashIntegration = true;
+      enableFishIntegration = true;
       settings = builtins.fromJSON (builtins.readFile "${self}/data/.ysomic.omp.json");
     };
 
@@ -48,6 +56,7 @@
     lsd = {
       enable = true;
       enableBashIntegration = true;
+      enableFishIntegration = true;
     };
 
     zoxide = {
@@ -57,14 +66,73 @@
       ];
       # We're doing this solo to put it at the end
       enableBashIntegration = false;
+      enableFishIntegration = true;
+    };
+
+    fish = {
+      enable = true;
+      shellAliases = {
+        "docker-compose" = "docker compose";
+        ":qa" = "exit";
+        ":wq" = "exit";
+        ":q" = "exit";
+      };
+
+      interactiveShellInit = ''
+        set fish_greeting
+      '';
+
+      functions = {
+        gfp.body = ''
+          git fetch
+          and git pull
+        '';
+
+        # gnb = {
+        #   argumentNames = "branch";
+        #   body = ''
+        #     if test -z "$branch"
+        #       echo "Usage: gnb <branch_name>"
+        #       return 1
+        #     end
+        #
+        #     git checkout -b "$branch"
+        #     or begin
+        #       echo "Failed to checkout to branch $branch"
+        #       return 1
+        #     end
+        #
+        #     git push -u origin "$branch"
+        #     or begin
+        #       echo "Failed to push the branch $branch to origin"
+        #       return 1
+        #     end
+        #
+        #     echo "Successfully checked out to and pushed $branch"
+        #   '';
+        # };
+      };
     };
 
     bash = {
-      enable = true;
+      enable = false;
       historySize = 2500;
       historyControl = ["ignoredups" "erasedups"];
 
+      # interactiveShellInit = ''
+      #   if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      #   then
+      #     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      #     exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      #   fi
+      # '';
+
       initExtra = lib.mkOrder 2000 ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
         eval "$(${pkgs.zoxide}/bin/zoxide init bash --cmd cd)"
       '';
 
