a258e1654d35525cfe1951a27d2cf08a3514fd28
MathieuDR <mathieu@deraedt.dev>
2025-08-05 15:38:28 +0200

    darwin init

---
+++ configuration/darwin/default.nix
@@ -0,0 +1,4 @@
+{...}: {
+  # Necessary for using flakes on this system
+  nix.settings.experimental-features = "nix-command flakes";
+}
--- darwin/imposter/default.nix
+++ darwin/imposter/default.nix
@@ -1,1 +1,17 @@
-{...}: {}
+{
+  inputs,
+  self,
+  hostname,
+  user,
+  alias,
+  ...
+}: {
+  # Host-specific Darwin configuration
+  environment.systemPackages = [
+  ];
+
+  system.defaults = {
+    dock.autohide = true;
+    finder.AppleShowAllExtensions = true;
+  };
+}
