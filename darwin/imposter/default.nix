6bb350d4b0dba80e7ceae81594aa1dbeb8629c38
MathieuDR <mathieu@deraedt.dev>
2025-08-06 11:28:33 +0200

    system

--- darwin/imposter/default.nix
+++ darwin/imposter/default.nix
@@ -11,38 +11,40 @@
     inputs.home-manager.packages.aarch64-darwin.default
   ];
 
-  system.primaryUser = user;
+  system = {
+    primaryUser = user;
+    startup.chime = false;
 
-  system.startup.chime = false;
-  system.defaults = {
-    ".GlobalPreferences"."com.apple.mouse.scaling" = -1.0;
-    NSGlobalDomain = {
-      AppleICUForce24HourTime = true;
-      AppleInterfaceStyle = "Dark";
-      AppleMeasurementUnits = "Centimeters";
-      AppleMetricUnits = 1;
-      AppleShowAllExtensions = true;
-      AppleShowAllFiles = true;
-      AppleTemperatureUnit = "Celsius";
-      NSDocumentSaveNewDocumentsToCloud = false;
-      NSTableViewDefaultSizeMode = 1;
-      "com.apple.springing.enabled" = false;
-      "com.apple.swipescrolldirection" = false;
-    };
-    WindowManager.GloballyEnabled = false;
-    dock.autohide = true;
-    finder = {
-      ShowPathbar = true;
-      ShowExternalHardDrivesOnDesktop = false;
-      FXRemoveOldTrashItems = true;
-      FXPreferredViewStyle = "Nlsv";
-      CreateDesktop = false;
-      AppleShowAllFiles = true;
-      AppleShowAllExtensions = true;
-    };
+    defaults = {
+      ".GlobalPreferences"."com.apple.mouse.scaling" = -1.0;
+      NSGlobalDomain = {
+        AppleICUForce24HourTime = true;
+        AppleInterfaceStyle = "Dark";
+        AppleMeasurementUnits = "Centimeters";
+        AppleMetricUnits = 1;
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        AppleTemperatureUnit = "Celsius";
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSTableViewDefaultSizeMode = 1;
+        "com.apple.springing.enabled" = false;
+        "com.apple.swipescrolldirection" = false;
+      };
+      WindowManager.GloballyEnabled = false;
+      dock.autohide = true;
+      finder = {
+        ShowPathbar = true;
+        ShowExternalHardDrivesOnDesktop = false;
+        FXRemoveOldTrashItems = true;
+        FXPreferredViewStyle = "Nlsv";
+        CreateDesktop = false;
+        AppleShowAllFiles = true;
+        AppleShowAllExtensions = true;
+      };
 
-    loginwindow.GuestEnabled = false;
-    screencapture.target = "clipboard";
+      loginwindow.GuestEnabled = false;
+      screencapture.target = "clipboard";
+    };
 
     keyboard = {
       enableKeyMapping = false;
