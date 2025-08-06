d1c475558d09e8cd39be8b72ae32cbfb02f9b488
MathieuDR <mathieu@deraedt.dev>
2025-08-06 11:30:52 +0200

    scripts

--- configuration/home-manager/scripts/default.nix
+++ configuration/home-manager/scripts/default.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   home.packages = [
-    (import ./rofi-systemd.nix {inherit pkgs;})
-    (import ./open_app_on_workspace.nix {inherit pkgs;})
+    # (import ./rofi-systemd.nix {inherit pkgs;})
+    # (import ./open_app_on_workspace.nix {inherit pkgs;})
     (import ./list_files_and_contents.nix {inherit pkgs;})
-    (import ./kiosk_in_window.nix {inherit pkgs;})
+    # (import ./kiosk_in_window.nix {inherit pkgs;})
     (import ./diff.nix {inherit pkgs;})
   ];
 }
