9c0d0863af73a3901498532f7421bf08aa93edbb
MathieuDR <mathieu@deraedt.dev>
2026-02-10 17:01:01 +0100

    zeit

--- pkgs/zeit.nix
+++ pkgs/zeit.nix
@@ -1,16 +1,16 @@
 {pkgs}:
 pkgs.buildGoModule rec {
   pname = "zeit";
-  version = "0.0.9";
+  version = "1.0.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "mrusme";
     repo = "zeit";
     rev = "v${version}";
-    sha256 = "sha256-CRECjhTHen4bIHZmQ2scL7cSuaxn5LKhok4dlXkOIpU=";
+    sha256 = "sha256-EfsH0U2zHnblK3f9ysmqpqyonQKPKr3tpjZgWWiORtE=";
   };
 
-  vendorHash = "sha256-SObUqLESRDyqqYzIIh9zdASXs6+uxHIZg7XMGn98mvk=";
+  vendorHash = "sha256-qhpWOb1VpEidsgaA3sMnvEl2RD/uR4cTaD8aT0fYOAU=";
 
   ldflags = [
     "-X github.com/mrusme/zeit/z.VERSION=${version}"
