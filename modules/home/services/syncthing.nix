1e6ea810fc63fbf6e01969b48177050d1fde515a
MathieuDR <mathieu@deraedt.dev>
2026-03-28 12:58:26 +0100

    syncthing

--- modules/home/services/syncthing.nix
+++ modules/home/services/syncthing.nix
@@ -1,9 +1,5 @@
 {...}: {
-  flake.modules.homeManager.syncthing = {
-    config,
-    lib,
-    ...
-  }: {
+  flake.modules.homeManager.syncthing = {config, ...}: {
     services.syncthing = {
       enable = true;
 
@@ -15,7 +11,7 @@
             id = "shared-secrets";
             label = "Shared Secrets";
             type = "sendreceive";
-            devices = ["wanderer" "mobile" "bastion"];
+            devices = ["wanderer" "mobile" "imposter" "bastion"];
             versioning = {
               type = "staggered";
               fsPath = "${config.home.homeDirectory}/secrets/backup-versions";
@@ -28,14 +24,25 @@
         };
 
         devices = {
+          # Tower
           "bastion" = {
             name = "bastion";
             id = "POE5SHE-34YJMHG-Q7FATME-7YYUVPH-SL2P4ZZ-45SHHN3-A3XBRWD-6GPTJQZ";
           };
+
+          # Mac
+          "imposter" = {
+            name = "imposter";
+            id = "HP4TW4P-PE6PJHT-J53MIOZ-6M4RADW-CXXJDAS-SEXRM3V-PFXOSGI-2GVRDQB";
+          };
+
+          # Pixel 6a
           "mobile" = {
             name = "mobile";
             id = "WT572IB-2LCXROQ-RKPC5CC-2GDP2KW-47EDGGP-KWCUJCE-XQBVUCB-3DNHLAS";
           };
+
+          # Pixel 9
           "wanderer" = {
             name = "wanderer";
             id = "2GPANBZ-QFOVSED-6F5W7M5-FYDAGL3-5YAO5MH-U5YOXTT-YKEBRVO-W2BHJAC";
